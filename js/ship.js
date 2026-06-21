b5a1b031fa3c36204c188f8f9c66d86ecea2ccc9
JoshDa777 <joshuadavidjdgc@hotmail.com>
2026-06-20 22:46:09 -0500

    Refactor createShip function formatting

--- js/ship.js
+++ js/ship.js
@@ -1,17 +1,17 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.179/build/three.module.js';
 
-export function createShip(){
+export function createShip() {
 
     const geometry = new THREE.ConeGeometry(
-        0.4,
-        1.4,
+        0.5,
+        1.5,
         3
     );
 
     const material = new THREE.MeshStandardMaterial({
-        color:0x66aaff,
-        emissive:0x2255ff,
-        emissiveIntensity:2
+        color: 0x66aaff,
+        emissive: 0x2255ff,
+        emissiveIntensity: 2
     });
 
     const ship = new THREE.Mesh(
