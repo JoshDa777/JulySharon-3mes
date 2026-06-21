c1c78d16daf04a15f99101457455cd87578d3487
JoshDa777 <joshuadavidjdgc@hotmail.com>
2026-06-20 22:15:40 -0500

    Add createShip function to generate a ship model

--- js/ship.js
+++ js/ship.js
@@ -1,1 +1,25 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.179/build/three.module.js';
 
+export function createShip(){
+
+    const geometry = new THREE.ConeGeometry(
+        0.4,
+        1.4,
+        3
+    );
+
+    const material = new THREE.MeshStandardMaterial({
+        color:0x66aaff,
+        emissive:0x2255ff,
+        emissiveIntensity:2
+    });
+
+    const ship = new THREE.Mesh(
+        geometry,
+        material
+    );
+
+    ship.rotation.z = Math.PI / 2;
+
+    return ship;
+}
